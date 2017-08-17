323f116fef6ac6ef68ca709fd65e0e5dfbf6a7c2
Potrik98 <potrik98@gmail.com>
2017-08-17 23:52:40 +0200

    Add getNextEvent for repeatable events

--- src/main/java/com/roervik/timetable/model/Event.java
+++ src/main/java/com/roervik/timetable/model/Event.java
@@ -6,12 +6,31 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 @Data
-@Builder
+@Builder(toBuilder = true)
 public class Event {
     private final UUID id;
     private final LocalDateTime startTime;
     private final LocalDateTime endTime;
     private final boolean repeat;
     private final RepeatSpec repeatSpec;
+
+    public Event getNextEvent(final LocalDateTime currentTime) {
+        if (!repeat) return this;
+        if (repeatSpec.getType() == RepeatSpec.RepeatType.DAILY) {
+            final long daysBetween = DAYS.between(startTime, currentTime);
+            final long daysToIncrement = currentTime.isBefore(startTime.plusDays(daysBetween))
+                    ? daysBetween
+                    : daysBetween + 1;
+            final LocalDateTime nextEventStartTime = startTime.plusDays(daysToIncrement);
+            final LocalDateTime nextEventEndTime = endTime.plusDays(daysToIncrement);
+            return this.toBuilder()
+                    .startTime(nextEventStartTime)
+                    .endTime(nextEventEndTime)
+                    .build();
+        }
+        return null;
+    }
 }
--- src/test/java/com/roervik/timetable/model/EventModelTest.java
+++ src/test/java/com/roervik/timetable/model/EventModelTest.java
@@ -20,7 +20,7 @@ public class EventModelTest {
 
     @Test
     public void testWriteModelToJson() throws IOException {
-        Event event = Event.builder()
+        final Event event = Event.builder()
                 .id(UUID.randomUUID())
                 .startTime(LocalDateTime.parse(startTime))
                 .endTime(LocalDateTime.parse(endTime))
@@ -35,8 +35,40 @@ public class EventModelTest {
                         .build())
                 .build();
 
-        String json = mapper.writeValueAsString(event);
-        Event parsedEvent = mapper.readValue(json, Event.class);
+        final String json = mapper.writeValueAsString(event);
+        final Event parsedEvent = mapper.readValue(json, Event.class);
+        System.out.println(json);
         assertThat(parsedEvent).isEqualTo(event);
+    }
+
+    @Test
+    public void testGetNextEventDaily() {
+        final Event event = Event.builder()
+                .id(UUID.randomUUID())
+                .startTime(LocalDateTime.parse(startTime))
+                .endTime(LocalDateTime.parse(endTime))
+                .repeat(true)
+                .repeatSpec(RepeatSpec.builder()
+                        .type(RepeatSpec.RepeatType.DAILY)
+                        .build())
+                .build();
+
+        final LocalDateTime currentTime = LocalDateTime.parse("2017-08-17T09:00:00");
+        final LocalDateTime expectedNextStartTime = LocalDateTime.parse("2017-08-17T13:15:00");
+        final LocalDateTime expectedNextEndTime = LocalDateTime.parse("2017-08-17T14:15:00");
+
+        final Event nextEvent = event.getNextEvent(currentTime);
+
+        assertThat(nextEvent.getStartTime()).isEqualTo(expectedNextStartTime);
+        assertThat(nextEvent.getEndTime()).isEqualTo(expectedNextEndTime);
+
+        final LocalDateTime currentTimeLateEvening = LocalDateTime.parse("2017-08-17T21:00:00");
+        final LocalDateTime expectedNextStartTimeTomorrow = LocalDateTime.parse("2017-08-18T13:15:00");
+        final LocalDateTime expectedNextEndTimeTomorrow = LocalDateTime.parse("2017-08-18T14:15:00");
+
+        final Event nextEventTomorrow = event.getNextEvent(currentTimeLateEvening);
+
+        assertThat(nextEventTomorrow.getStartTime()).isEqualTo(expectedNextStartTimeTomorrow);
+        assertThat(nextEventTomorrow.getEndTime()).isEqualTo(expectedNextEndTimeTomorrow);
     }
 }
